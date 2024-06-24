214edd78eaf4c1ac28b7145593efecab1a57abbc
Ivan Minaev <minaev70ivan009@gmail.com>
2024-06-24 22:03:30 +0300

    коммит на всякий случай

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,49 +1,54 @@
-const starsConteiner = document.querySelector(".NUE-rate__stars");
-const stars = document.querySelectorAll(".NUE-rate__star");
+// const starsConteiner = document.querySelector(".NUE-rate__stars");
+// const stars = document.querySelectorAll(".NUE-rate__star");
 
-starsConteiner.addEventListener("click", (e) => {
-	if (e.target.closest(".NUE-rate__star")) {
-		if (e.target.closest(".NUE-rate__not-active-star")) {
-			const indexStar = e.target.id.split("index")[1];
-			for (let i = 0; i < indexStar; i++) {
-				stars[i].src = e.target.src = "../img/star.svg";
-				stars[i].classList.remove("NUE-rate__not-active-star");
-				stars[i].alt = "звезда";
-			}
-		} else {
-			const indexStar = e.target.id.split("index")[1];
-			for (let i = stars.length - 1; i >= indexStar; i--) {
-				stars[i].src = "../img/starNotActive.svg";
-				stars[i].classList.add("NUE-rate__not-active-star");
-				stars[i].alt = "не активная звезда";
-			}
-		}
-	}
-});
+// starsConteiner.addEventListener("click", (e) => {
+// 	if (e.target.closest(".NUE-rate__star")) {
+// 		if (e.target.closest(".NUE-rate__not-active-star")) {
+// 			const indexStar = e.target.id.split("index")[1];
+// 			for (let i = 0; i < indexStar; i++) {
+// 				stars[i].src = e.target.src = "../img/star.svg";
+// 				stars[i].classList.remove("NUE-rate__not-active-star");
+// 				stars[i].alt = "звезда";
+// 			}
+// 		} else {
+// 			const indexStar = e.target.id.split("index")[1];
+// 			for (let i = stars.length - 1; i >= indexStar; i--) {
+// 				stars[i].src = "../img/starNotActive.svg";
+// 				stars[i].classList.add("NUE-rate__not-active-star");
+// 				stars[i].alt = "не активная звезда";
+// 			}
+// 		}
+// 	}
+// });
 
-const input = document.querySelector(".NUE-sub__input-email");
-const inputWrapp = document.querySelector(".NUE-sub__input-wrapp");
+// const input = document.querySelector(".NUE-sub__input-email");
+// const inputWrapp = document.querySelector(".NUE-sub__input-wrapp");
 
-input.addEventListener("focus", () => {
-	if (window.innerWidth <= 680) {
-		input.previousElementSibling.style =
-			"min-width: 0px; width:0px; max-height:22px";
-	} else {
-		input.previousElementSibling.style =
-			"min-width: 0px; width:0px; max-height:27px";
-	}
-	input.style = "border-left: none";
-	inputWrapp.style = "gap:0px";
-});
-input.addEventListener("blur", () => {
-	if (window.innerWidth <= 680) {
-		input.previousElementSibling.style = "max-height:22px";
-	} else {
-		input.previousElementSibling.style = "max-height:27px";
-	}
-	setTimeout(() => {
-		input.previousElementSibling.style = "";
-	}, 300);
-	input.style = "";
-	inputWrapp.style = "";
+// input.addEventListener("focus", () => {
+// 	if (window.innerWidth <= 680) {
+// 		input.previousElementSibling.style = "min-width: 0px; width:0px; max-height:22px";
+// 	} else {
+// 		input.previousElementSibling.style = "min-width: 0px; width:0px; max-height:27px";
+// 	}
+// 	input.style = "border-left: none";
+// 	inputWrapp.style = "gap:0px";
+// });
+// input.addEventListener("blur", () => {
+// 	if (window.innerWidth <= 680) {
+// 		input.previousElementSibling.style = "max-height:22px";
+// 	} else {
+// 		input.previousElementSibling.style = "max-height:27px";
+// 	}
+// 	setTimeout(() => {
+// 		input.previousElementSibling.style = "";
+// 	}, 300);
+// 	input.style = "";
+// 	inputWrapp.style = "";
+// });
+
+const categoriesBt = document.querySelector(".catalog__categories-bt");
+
+categoriesBt.addEventListener("click", () => {
+	document.querySelector(".catalog__categories-list").classList.toggle("active");
+	document.querySelector(".catalog__categories-bt").classList.toggle("active");
 });
